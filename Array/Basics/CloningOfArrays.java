4d8519794e8b599cedd2ea9419dd5355b86c9a31
Ananth <mvananthkumar@gmail.com>
2022-06-11 23:50:23 +0530

    Commit

---
+++ Array/Basics/CloningOfArrays.java
@@ -0,0 +1,10 @@
+public class CloningOfArrays {
+    public static void main(String[] args) {
+        int intArray[] = {1,2,3};
+
+        int cloneArray[] = intArray.clone();
+
+        // will print false as deep copy is created 
+        // for one-dimensional array
+    }
+}
--- Array/Basics/multiDimensional.java
+++ Array/Basics/multiDimensional.java
@@ -2,19 +2,16 @@ import java.util.ArrayList;
 
 public class multiDimensional {
     public static void main(String[] args) {
-        int arr1[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int arr1[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-
-        for (int i=0; i< arr1.length ; i++)
-        {
-            for (int j=0; j < arr1[i].length ; j++)
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++)
                 System.out.print(arr1[i][j] + " ");
- 
+
             System.out.println();
         }
 
         ArrayList<ArrayList<Integer>> arr2 = new ArrayList<ArrayList<Integer>>();
-
 
     }
 }
